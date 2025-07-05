cd6f199e4272d9c49c01901bba9d8678d8c5b8e8
Kjeld Schmidt <kjeld.schmidt@the-coding-kind.com>
2025-07-05 18:54:14 +0200

    feat: circles, rainbow break
    Co-Authored-By: Judith Holve (small-mass@space)

--- main.py
+++ main.py
@@ -16,19 +16,39 @@ class Velocity:
         self.x = x
         self.y = y
 
+    def __repr__(self):
+        return f"Velocity ({self.x}, {self.y})"
+
 class ShellPoint:
-    def __init__(self, x: float, y: float, v: Velocity, c: Point):
+    def __init__(self, x: float, y: float, c: Point, f_c: float):
         self.x = x
         self.y = y
-        self.v = v
-        self.mass = 1
+        self.m = 1
+        self.f_c = f_c
         self.c = c
-        self.r = ((x-c.x)**2 + (y-c.y)**2)**0.5
+        self.v = self.calculate_initial_stable_velocity()
+        self.rainbow = rainbow()
 
-    def update(self):
+    def calculate_initial_stable_velocity(self):
         direction_to_center = self.c.x - self.x, self.c.y - self.y
         h = (direction_to_center[0]**2 + direction_to_center[1]**2)**0.5
-        force = 1
+
+        f_c_x = (direction_to_center[0]/h)**2 * self.f_c
+        f_c_y = (direction_to_center[1]/h)**2 * self.f_c
+
+
+        r = ((self.x-self.c.x)**2 + (self.y-self.c.y)**2)**0.5
+        v_x = ((abs(f_c_x)/self.m) * r) ** 0.5
+        v_y = ((abs(f_c_y)/self.m) * r) ** 0.5
+
+        return Velocity(v_y, -v_x)
+
+
+    def update(self):
+        # print(f"Before: {self.x}")
+        direction_to_center = self.c.x - self.x, self.c.y - self.y
+        h = (direction_to_center[0]**2 + direction_to_center[1]**2)**0.5
+        force = self.f_c
 
         a_c_x = direction_to_center[0]/h * force
         a_c_y = direction_to_center[1]/h * force
@@ -38,9 +58,11 @@ class ShellPoint:
 
         self.x += self.v.x
         self.y += self.v.y
+        # print(f"After: {self.x}")
 
     def draw(self):
-        app.fill(220, 13, 35)
+        app.fill(*next(self.rainbow))
+        app.noStroke()
         app.circle(self.x, self.y, 5)
 
 
@@ -59,23 +81,37 @@ class SmallMass:
         app.fill(40, 35, 180)
         app.circle(self.x, self.y, self.m*10)
 
+def rainbow():
+    h = 0
+    inc = 0.0025
+    while True:
+        yield h, 1, 1
+        h += inc
+        if h >= 1:
+            h %= 1
+
 c = Point(width/2, height/2)
 
 judy = SmallMass(c.x, c.y + 100, 1, Velocity(1, 0))
-shell = [ShellPoint(300, 300, Velocity(5, 0), c)]
+f_c = 0.1
+
+shell = [
+    ShellPoint(c.x, y, c, f_c) for y in range(15, 400, 7)
+]
 
 def draw():
-    app.background(255)
-    app.fill(0)
     app.ellipse(c.x, c.y, 5, 5)
     judy.update()
-    judy.draw()
+    # judy.draw()
     for shell_point in shell:
         shell_point.update()
         shell_point.draw()
 
+app.background(255)
+app.fill(0)
+app.colorMode(HSB, 1, 1, 1)
 while True:
-   draw()
-   app.redraw() # refresh the window
+    draw()
+    app.redraw() # refresh the window
 
 
